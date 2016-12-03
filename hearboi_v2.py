4d4322e3d2e18f57b73b2784f94257d597ec9f53
tchesebro <tchesebro@berkeley.edu>
2016-12-03 00:45:53 +0000

    thank god

--- hearboi_v2.py
+++ hearboi_v2.py
@@ -28,7 +28,7 @@ def monitor(threadname):
 
 def add_new_sound(audio, frames, info, name):
     FORMAT = info['FORMAT']
-    CHANNELS = info['INFO']
+    CHANNELS = info['CHANNELS']
     RATE = info['RATE']
     CHUNK = info['CHUNK']
     RECORD_SECONDS = info['RECORD_SECONDS']
@@ -56,6 +56,15 @@ def main(threadname):
     'WAVE_OUTPUT_FILENAME' : "recording.wav",
     'THRESHOLD' : 1000
     }
+    frames = []
+    FORMAT = info['FORMAT']
+    CHANNELS = info['CHANNELS']
+    RATE = info['RATE']
+    CHUNK = info['CHUNK']
+    RECORD_SECONDS = info['RECORD_SECONDS']
+    WAVE_OUTPUT_FILENAME = info['WAVE_OUTPUT_FILENAME']
+    THRESHOLD = info['THRESHOLD']
+
 
     audio = pyaudio.PyAudio()
     confidence_threshold = 5
@@ -69,22 +78,6 @@ def main(threadname):
             "db": "sound_db",
         }
     }
-
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
-                    frames_per_buffer=CHUNK,
-            input_device_index=4)
-    print "listening..."
-
-    frames = []
-    FORMAT = info['FORMAT']
-    CHANNELS = info['INFO']
-    RATE = info['RATE']
-    CHUNK = info['CHUNK']
-    RECORD_SECONDS = info['RECORD_SECONDS']
-    WAVE_OUTPUT_FILENAME = info['WAVE_OUTPUT_FILENAME']
-    THRESHOLD = info['THRESHOLD']
-
     # create a Dejavu instance
     djv = Dejavu(config)
 
@@ -96,42 +89,59 @@ def main(threadname):
 
     record = False
     while True:
+	    stream = audio.open(format=FORMAT, channels=CHANNELS,
+			    rate=RATE, input=True,
+			    frames_per_buffer=CHUNK,
+		    input_device_index=4)
+	    print "listening..."
 
-        # while status=='STOP':
-        #     time.sleep(1.5)
+	while True:
 
-        data = stream.read(CHUNK)
-        level = audioop.rms(data, 2)
-        #print "%s" % level
-        if level > THRESHOLD or record:
+		# while status=='STOP':
+		#     time.sleep(1.5)
 
-            print "recording"
-            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-                data = stream.read(CHUNK)
-                frames.append(data)
+		data = stream.read(CHUNK)
+		print 'streaming'
+		level = audioop.rms(data, 2)
+		#print "%s" % level
+		if level > THRESHOLD or record:
+		    frames = []
+		    print "recording"
+		    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+			data = stream.read(CHUNK)
+			frames.append(data)
 
-            if status=='START':
-                # name = get from server
-                add_new_sound(audio, frames, info, filename)
+		    if status=='START':
+			# name = get from server
+			add_new_sound(audio, frames, info, filename)
 
-                urllib2.urlopen("http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/record/stop")
-                files = {'audioFile': open(filename, 'rb')}
-                uploadSite = 'http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/record/upload'
-                r = requests.post(uploadSite, files=files)
+			urllib2.urlopen("http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/record/stop")
+			files = {'audioFile': open(filename, 'rb')}
+			uploadSite = 'http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/record/upload'
+			r = requests.post(uploadSite, files=files)
 
-            else:
-                add_new_sound(frames, info, info['WAVE_OUTPUT_FILENAME'])
-                print "finished recording"
+		    else:
+			add_new_sound(audio, frames, info, info['WAVE_OUTPUT_FILENAME'])
+			print "finished recording"
 
-                song = djv.recognize(FileRecognizer, "recording.wav")
-                song_name = song['song_name']
-                if song['confidence'] > confidence_threshold:
-                    print "From file we recognized: %s\n" % song
+			song = djv.recognize(FileRecognizer, "recording.wav")
+			try:
+				song_name = song['song_name']
+				song_conf = song['confidence']
+				print 'song return:', song
+			except:
+				song_name = None
+				song_conf = 0
+			if song_conf > confidence_threshold:
+			    print "From file we recognized: %s\n" % song
 
-                    print "Alerting with SMS"
-                    website = 'http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/sendSMS/4'
-                    urllib2.urlopen(website).read()
-
+			    print "Alerting with SMS"
+			    website = 'http://ec2-54-71-180-108.us-west-2.compute.amazonaws.com/hearboi/device/sendSMS/4'
+			    urllib2.urlopen(website).read()
+		    stream.stop_stream()
+		    stream.close()
+		    audio.terminate()
+		    break
 
 if __name__=='__main__':
     thread.start_new_thread(monitor,("monitor thread",))
